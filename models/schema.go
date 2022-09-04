01d876695c9066fa9ceb2b7812ead4f3493a533d
VJ-Vijay77 <vijaydinesh77vj@gmail.com>
2022-09-04 23:36:33 +0530

    changes

--- models/schema.go
+++ models/schema.go
@@ -1,13 +1,6 @@
 package models
 
 
-var UserTable = `CREATE TABLE person(
-	firstname text,
-	lastname text,
-	email text,
-	password text,
-	age integer);`
-
 
 type Users struct {
 	FirstName string `json:"firstname"`
