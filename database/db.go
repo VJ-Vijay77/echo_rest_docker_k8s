ed0965655bbf83acde5680def479b8f2886a61ea
VJ-Vijay77 <vijaydinesh77vj@gmail.com>
2022-09-04 21:20:00 +0530

    small change

--- database/db.go
+++ database/db.go
@@ -8,7 +8,7 @@ import (
 )
 
 func ConnectDB() *sqlx.DB {
-	DSN := "postgresql://vijay:zmxmcmvbn@172.28.0.2:5432/echo_rest?sslmode=disable"
+	DSN := "postgresql://vijay:zmxmcmvbn@postgres:5432/echo_rest?sslmode=disable"
 	db, err := sqlx.Connect("postgres", DSN)
 	if err != nil {
 		log.Fatalln(err)
