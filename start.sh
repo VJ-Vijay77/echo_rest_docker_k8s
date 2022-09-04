d00c3e8bcc05dcf719d9124d509cd331eee0a6f8
VJ-Vijay77 <vijaydinesh77vj@gmail.com>
2022-09-05 00:17:52 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
-#!bin/sh
+#!/bin/sh
 
- 
+set -e
+
+echo "running db migration"
+/app/migrate -path /app/migration -database "postgresql://vijay:zmxmcmvbn@postgres:5432/echo_rest?sslmode=disable" -verbose up
+
+echo "Starting app"
+
+exec "$@"
